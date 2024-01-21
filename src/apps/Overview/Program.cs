f87e0ce3f3a7a5f589b5b75f2149dd3395e72325
Grant Barnes <gfbarnes78@gmail.com>
2024-01-20 20:12:51 -0600

    adding cpu details to Overview

--- src/apps/Overview/Program.cs
+++ src/apps/Overview/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Overview
 
+using System.Text;
+
 Console.WriteLine($"    User: {GetUser()}");
 Console.WriteLine($"Hostname: {GetHostname()}");
 Console.WriteLine($"      OS: {GetOS()}");
@@ -60,12 +62,85 @@ static string GetKernel()
 
 static string GetCPU()
 {
-    throw new NotImplementedException();
+    var lines = File.ReadAllLines("/proc/cpuinfo");
+    foreach (var line in lines)
+    {
+        if (line.StartsWith("model name"))
+        {
+            return line.Split(": ").Last().Trim();
+        }
+    }
+    return unknown;
 }
 
 static string GetSpeed()
 {
-    throw new NotImplementedException();
+    var processorSpeeds = new List<float>();
+    var currentSpeed = 0.0;
+    var maxSpeed = 0.0;
+
+    var cpuInfoLines = File.ReadAllLines("/proc/cpuinfo");
+    foreach (var line in cpuInfoLines)
+    {
+        if (line.StartsWith("cpu MHz"))
+        {
+            var speedString = line.Split(": ").Last().Trim();
+            if (float.TryParse(speedString, out float speedFloat))
+            {
+                processorSpeeds.Add(speedFloat);
+            }
+        }
+    }
+
+    if (processorSpeeds.Count > 0)
+    {
+        var sum = processorSpeeds.Sum();
+        currentSpeed = sum / processorSpeeds.Count / 1000;
+    }
+
+    string[] files = [
+        "/sys/devices/system/cpu/cpu0/cpufreq/bios_limit",
+        "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq",
+        "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
+    ];
+    foreach (var file in files)
+    {
+        try
+        {
+            var lines = File.ReadAllLines(file);
+            foreach (var line in lines)
+            {
+                if (char.IsNumber(line.First()))
+                {
+                    maxSpeed = float.Parse(line) / 1000 / 1000;
+                }
+            }
+        }
+        catch { }
+    }
+
+    var result = new StringBuilder();
+    if (currentSpeed > 0)
+    {
+        result.Append($"{Math.Round(currentSpeed, 2)} GHz");
+    }
+
+    if (maxSpeed > 0)
+    {
+        if (result.Length > 0)
+        {
+            result.Append(" / ");
+        }
+        result.Append($"{Math.Round(maxSpeed, 2)} GHz");
+
+        var percentage = currentSpeed / maxSpeed * 100;
+        if (percentage > 0)
+        {
+            result.Append($" ({Math.Round(percentage, 2)}%)");
+        }
+    }
+
+    return result.ToString();
 }
 
 static string GetMemory()
