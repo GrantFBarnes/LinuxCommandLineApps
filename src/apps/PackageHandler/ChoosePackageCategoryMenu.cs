ddf95d6fd82d619751e36b43aef342d5433ae242
Grant Barnes <gfbarnes78@gmail.com>
2024-01-25 19:46:02 -0600

    adding server packages

--- src/apps/PackageHandler/ChoosePackageCategoryMenu.cs
+++ src/apps/PackageHandler/ChoosePackageCategoryMenu.cs
@@ -39,6 +39,58 @@ internal sealed class ChoosePackageCategoryMenu(Distribution distribution)
             [
                 new Package()
                 {
+                    Name = "Cockpit - Web Interface",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["cockpit"] },
+                        { Repository.Debian, ["cockpit"] },
+                        { Repository.Fedora, ["cockpit"] },
+                        { Repository.RedHat, ["cockpit"] },
+                        { Repository.Ubuntu, ["cockpit"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "cURL - Client URL",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["curl"] },
+                        { Repository.Debian, ["curl"] },
+                        { Repository.Fedora, ["curl"] },
+                        { Repository.RedHat, ["curl"] },
+                        { Repository.Ubuntu, ["curl"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "dotnet - C# runtime 8.0 LTS",
+                    DesktopEnvironment = null,
+
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["dotnet-runtime-8.0"] },
+                        { Repository.Debian, ["dotnet-runtime-8.0"] },
+                        { Repository.Fedora, ["dotnet-runtime-8.0"] },
+                        { Repository.RedHat, ["dotnet-runtime-8.0"] },
+                        { Repository.Ubuntu, ["dotnet-runtime-8.0"] },
+                    },
+                    Flatpak = null,
+                    Snap = new Snap("dotnet-runtime-80", true, false),
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
                     Name = "dotnet - C# SDK 8.0 LTS",
                     DesktopEnvironment = null,
                     Repositories = new Dictionary<Repository, List<string>>()
@@ -73,7 +125,51 @@ internal sealed class ChoosePackageCategoryMenu(Distribution distribution)
                 },
                 new Package()
                 {
-                    Name = "htop",
+                    Name = "Flutter",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>(),
+                    Flatpak = null,
+                    Snap = new Snap("flutter", true, true),
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "git - Version Control",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["git"] },
+                        { Repository.Debian, ["git"] },
+                        { Repository.Fedora, ["git"] },
+                        { Repository.RedHat, ["git"] },
+                        { Repository.Ubuntu, ["git"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "Go Language",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["go", "gopls"] },
+                        { Repository.Debian, ["golang", "gopls"] },
+                        { Repository.Fedora, ["golang", "golang-x-tools-gopls"] },
+                        { Repository.RedHat, ["golang"] },
+                        { Repository.Ubuntu, ["golang", "gopls"] },
+                    },
+                    Flatpak = null,
+                    Snap = new Snap("go", true, true),
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "htop - Process Reviewer",
                     DesktopEnvironment = null,
                     Repositories = new Dictionary<Repository, List<string>>()
                     {
@@ -82,6 +178,187 @@ internal sealed class ChoosePackageCategoryMenu(Distribution distribution)
                         { Repository.Fedora, ["htop"] },
                         { Repository.RedHat, ["htop"] },
                         { Repository.Ubuntu, ["htop"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "MariaDB - Database",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["mariadb"] },
+                        { Repository.Debian, ["mariadb-server"] },
+                        { Repository.Fedora, ["mariadb-server"] },
+                        { Repository.RedHat, ["mariadb-server"] },
+                        { Repository.Ubuntu, ["mariadb-server"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "neovim - Text Editor",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        {
+                            Repository.Arch,
+                            ["neovim", "vim", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter", "vim-nerdtree"]
+                        },
+                        {
+                            Repository.Debian,
+                            ["neovim", "vim", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter"]
+                        },
+                        {
+                            Repository.Fedora,
+                            [
+                                "neovim", "vim-enhanced", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter",
+                                "vim-nerdtree"
+                            ]
+                        },
+                        {
+                            Repository.Ubuntu,
+                            ["neovim", "vim", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter"]
+                        },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "nano - Text Editor",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["nano"] },
+                        { Repository.Debian, ["nano"] },
+                        { Repository.Fedora, ["nano"] },
+                        { Repository.RedHat, ["nano"] },
+                        { Repository.Ubuntu, ["nano"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "Node.js - JavaScript RE",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["nodejs", "npm"] },
+                        { Repository.Debian, ["nodejs", "npm"] },
+                        { Repository.Fedora, ["nodejs", "npm"] },
+                        { Repository.RedHat, ["nodejs", "npm"] },
+                        { Repository.Ubuntu, ["nodejs", "npm"] },
+                    },
+                    Flatpak = null,
+                    Snap = new Snap("node", true, true, "18/stable"),
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "Podman - Containers",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["podman"] },
+                        { Repository.Debian, ["podman"] },
+                        { Repository.Fedora, ["podman"] },
+                        { Repository.RedHat, ["podman"] },
+                        { Repository.Ubuntu, ["podman"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "Rust Language",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["rustup"] },
+                        { Repository.Debian, ["rustc", "rustfmt", "cargo"] },
+                        { Repository.Fedora, ["rust", "rustfmt", "cargo"] },
+                        { Repository.RedHat, ["rust", "rustfmt", "cargo"] },
+                        { Repository.Ubuntu, ["rustc", "rustfmt", "cargo"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "Snap",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Debian, ["snapd"] },
+                        { Repository.Fedora, ["snapd"] },
+                        { Repository.RedHat, ["snapd"] },
+                        { Repository.Ubuntu, ["snapd"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "SSH - Secure Shell Protocol",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        { Repository.Arch, ["libssh", "openssh"] },
+                        { Repository.Debian, ["ssh"] },
+                        { Repository.Fedora, ["libssh", "openssh"] },
+                        { Repository.RedHat, ["libssh", "openssh"] },
+                        { Repository.Ubuntu, ["ssh"] },
+                    },
+                    Flatpak = null,
+                    Snap = null,
+                    PreInstall = null,
+                    PostInstall = null,
+                },
+                new Package()
+                {
+                    Name = "vim - Text Editor",
+                    DesktopEnvironment = null,
+                    Repositories = new Dictionary<Repository, List<string>>()
+                    {
+                        {
+                            Repository.Arch,
+                            ["vim", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter", "vim-nerdtree"]
+                        },
+                        {
+                            Repository.Debian,
+                            ["vim", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter"]
+                        },
+                        {
+                            Repository.Fedora,
+                            ["vim-enhanced", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter", "vim-nerdtree"]
+                        },
+                        {
+                            Repository.RedHat,
+                            ["vim-enhanced"]
+                        },
+                        {
+                            Repository.Ubuntu,
+                            ["vim", "vim-airline", "vim-ale", "vim-ctrlp", "vim-gitgutter"]
+                        },
                     },
                     Flatpak = null,
                     Snap = null,
